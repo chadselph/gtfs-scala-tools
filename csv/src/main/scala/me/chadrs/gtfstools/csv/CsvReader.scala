0175ae44ca5bbc8d111883ccbac09fade53847fd
chad <chad.selph@goswift.ly>
2020-05-30 22:18:21 -0700

    moved anonymous CsvCursor into class IndexSeqCsvCursor

--- csv/src/main/scala/me/chadrs/gtfstools/csv/CsvReader.scala
+++ csv/src/main/scala/me/chadrs/gtfstools/csv/CsvReader.scala
@@ -8,38 +8,14 @@ import me.chadrs.gtfstools.csv.CsvReader.Result
 import scala.util.Try
 
 trait CsvReader[F] {
-  def readFile(file: CsvFile) = {
+  def readFile(file: CsvFile): Seq[Result[F]] = {
     val indices = file.headers.zipWithIndex.toMap
-    def getFromRow(key: String, row: Seq[String], rowNum: Int): Option[Either[String, String]] =
-      indices.get(key).map { index =>
-        Either.cond(index < row.length, row(index), s"Line #$rowNum: index $index out of bounds")
-      }
     file.rows.zipWithIndex.map {
       case (row, rowNum) =>
         // TODO get extra columns
-        readRow(new CsvCursor {
-          override def required[T: CsvFromString](key: String): CsvReader.Result[T] =
-            getFromRow(key, row, rowNum + 2)
-              .getOrElse(Left(s"no such column $key"))
-              .flatMap(readValue(_))
-              // required fields don't allow empty, turn None to left
-              .flatMap(opt => opt.toRight("empty"))
-              .left
-              .map(error => s"Row ${rowNum + 2} column $key: $error")
-          override def optionally[T: CsvFromString](key: String): CsvReader.Result[Option[T]] = {
-            getFromRow(key, row, rowNum + 2)
-              .map(_.flatMap(readValue(_)))
-              // optional fields allow missing columns. turn None to Right
-              .getOrElse(Right(None))
-              .left
-              .map(error => s"Row ${rowNum + 2} column $key: $error")
-          }
-        })
+        readRow(new IndexSeqCsvCursor(indices, row.toIndexedSeq, rowNum + 2))
     }
   }
-
-  protected def readValue[T](s: String)(implicit reader: CsvFromString[T]): Result[Option[T]] =
-    reader.fromString(s)
 
   def readRow(getValue: CsvCursor): CsvReader.Result[F]
 }
@@ -49,7 +25,8 @@ object CsvReader {
   type Error = String
   type Result[T] = Either[Error, T]
 
-  def parseAs[T](file: CsvFile)(implicit reader: CsvReader[T]): Seq[Result[T]] = reader.readFile(file)
+  def parseAs[T](file: CsvFile)(implicit reader: CsvReader[T]): Seq[Result[T]] =
+    reader.readFile(file)
 
 }
 
@@ -71,41 +48,75 @@ trait CsvCursor {
   def optionally[T: CsvFromString](key: String): CsvReader.Result[Option[T]]
 }
 
+class IndexSeqCsvCursor(indices: Map[String, Int], row: IndexedSeq[String], rowNumber: Int)
+    extends CsvCursor {
+
+  override def required[T: CsvFromString](key: String): CsvReader.Result[T] =
+    getFromRow(key, row, rowNumber)
+      .getOrElse(Left(s"no such column $key"))
+      .flatMap(readValue(_))
+      // required fields don't allow empty, turn None to left
+      .flatMap(opt => opt.toRight("empty"))
+      .left
+      .map(error => s"Row $rowNumber column $key: $error")
+  override def optionally[T: CsvFromString](key: String): CsvReader.Result[Option[T]] = {
+    getFromRow(key, row, rowNumber)
+      .map(_.flatMap(readValue(_)))
+      // optional fields allow missing columns. turn None to Right
+      .getOrElse(Right(None))
+      .left
+      .map(error => s"Row $rowNumber column $key: $error")
+  }
+  protected def readValue[T](s: String)(implicit reader: CsvFromString[T]): Result[Option[T]] =
+    reader.fromString(s)
+
+  private def getFromRow(
+      key: String,
+      row: Seq[String],
+      rowNum: Int
+  ): Option[Either[String, String]] =
+    indices.get(key).map { index =>
+      Either.cond(index < row.length, row(index), s"Line #$rowNum: index $index out of bounds")
+    }
+
+}
+
 object CsvFromString {
 
-  implicit val stringFromString: CsvFromString[String] = s => Right(Option(s.trim).filter(_.nonEmpty))
+  implicit val stringfromString: CsvFromString[String] = s => Right(Option(s).filter(_.nonEmpty))
+  val fromTrimmedString: CsvFromString[String] = stringfromString.map(_.trim)
 
   implicit val intFromString: CsvFromString[Int] =
-    stringFromString.flatmapF(
+    fromTrimmedString.flatmapF(
       nonEmptyStr => Either.cond(nonEmptyStr.forall(_.isDigit), nonEmptyStr.toInt, "not a number")
     )
 
   implicit val doubleFromString: CsvFromString[Double] =
-    stringFromString.flatmapF(
+    fromTrimmedString.flatmapF(
       nonEmptyStr => nonEmptyStr.toDoubleOption.toRight("not a floating point number")
     )
 
-  implicit val boolFromString: CsvFromString[Boolean] = stringFromString.flatmapF {
+  implicit val boolFromString: CsvFromString[Boolean] = fromTrimmedString.flatmapF {
     case "1" => Right(true)
     case "0" => Right(false)
     case _   => Left("Expected 1 or 0")
   }
 
-  implicit val urlFromString: CsvFromString[java.net.URI] = stringFromString.flatmapF { s =>
+  implicit val urlFromString: CsvFromString[java.net.URI] = fromTrimmedString.flatmapF { s =>
     Try(java.net.URI.create(s)).toEither.left.map(_ => "Not a URL")
   }
 
   private val gtfsDateFormat = DateTimeFormatter.ofPattern("yyyYMMdd")
-  implicit val localDateFromString: CsvFromString[LocalDate] = stringFromString.flatmapF { s =>
+  implicit val localDateFromString: CsvFromString[LocalDate] = fromTrimmedString.flatmapF { s =>
     Try(LocalDate.parse(s, gtfsDateFormat)).toEither.left.map(_ => "Not a date in YYYYMMDD format")
   }
   private val gtfsTimeFormat = DateTimeFormatter.ofPattern("H:mm:ss")
-  implicit val localTimeFromString: CsvFromString[LocalTime] = stringFromString.flatmapF { s =>
+  implicit val localTimeFromString: CsvFromString[LocalTime] = fromTrimmedString.flatmapF { s =>
     Try(LocalTime.parse(s, gtfsTimeFormat)).toEither.left
       .map(_ => s"$s is not a time in Hmmss format")
   }
 
-  implicit val zoneIdFromString: CsvFromString[java.time.ZoneId] = stringFromString.flatmapF { s =>
+  implicit val zoneIdFromString: CsvFromString[java.time.ZoneId] = fromTrimmedString.flatmapF { s =>
     Try(java.time.ZoneId.of(s)).toEither.left.map(_ => s"$s is not a valid timezone")
   }
 
